adb3cb23273dda53bb840b6687fc6dc4e4c95d1c
Jose Miguel Lueiza Valenzuela <jose.lueiza@lan.com>
2017-04-19 16:31:23 -0300

    bodyparser

--- src/auth.js
+++ src/auth.js
@@ -1,12 +1,15 @@
 import express from 'express';
+import bodyParser from 'body-parser';  
 
 
 const app = express();
-
+app.use(bodyParser.json());  
+app.use(bodyParser.urlencoded({extended: true}));  
+app.set('port', 3000);
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port '+app.get('port')+'!');
 });
